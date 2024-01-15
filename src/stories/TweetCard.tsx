f33907a46651731f1e2e7a5b38bee06a58d51904
Toshiki Matsuoka <ruirairin@outlook.jp>
2024-01-15 20:24:37 +0900

    tweet-card ui装

--- src/stories/TweetCard.tsx
+++ src/stories/TweetCard.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react'
 import { BsFillPatchCheckFill } from 'react-icons/bs'
+import { FaRegComment } from 'react-icons/fa'
+import { FaRetweet } from 'react-icons/fa'
+import { MdFavoriteBorder } from 'react-icons/md'
+import { ImParagraphLeft } from 'react-icons/im'
+import { CiBookmark } from 'react-icons/ci'
+import { LuShare } from 'react-icons/lu'
 
 type Props = { userName: string }
 
@@ -8,10 +14,17 @@ type Props = { userName: string }
  */
 export const TweetCard: FC<Props> = (userName) => {
   return (
-    <div className="bg-black text-white">
-      <div>
-        <div></div>
-        <div>
+    <div className="border-b-[1px] border-gray-400 bg-black px-[10px] pt-[10px] text-white">
+      <div className="flex">
+        <div className="mr-[10px]">
+          <img
+            src="./frame1.png"
+            height="40px"
+            width="40px"
+            className="rounded-full"
+          />
+        </div>
+        <div className="w-full">
           <div className="flex items-center text-[15px]">
             <div className="pr-[5px] font-bold">Reactおじさん</div>
             <div className="flex items-center">
@@ -24,6 +37,16 @@ export const TweetCard: FC<Props> = (userName) => {
             ずん喫茶の録画みてたら
             <br />
             猛烈にピラフが食べたくなってきた・・・！
+          </div>
+          <div className="flex w-full justify-between py-[10px]">
+            <FaRegComment />
+            <FaRetweet />
+            <MdFavoriteBorder />
+            <ImParagraphLeft />
+            <div className="flex">
+              <CiBookmark className="mr-[25px]" />
+              <LuShare />
+            </div>
           </div>
         </div>
       </div>
